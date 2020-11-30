9e95889039c82c894815e895f5be568f26731864
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-11-30 16:59:26 -0500

    Template-Driven Form - Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnChanges } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('form', { static: false }) subsForm: NgForm;
+  defaultSub = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.subsForm.value.email;
+    this.user.subscription = this.subsForm.value.subs;
+    this.user.password = this.subsForm.value.passw;
+    console.log(this.subsForm.value);
+  }
 }
